b8d0c09eb586cd7b502bb64c4ef15a38cf69015e
ljeancarlos885-gif <ljeancarlos885@gmail.com>
2025-10-14 12:00:20 -0500

    Add files via upload

---
+++ programacion orientada a  objetos/modelo_hilo.py
@@ -0,0 +1,14 @@
+"""clase Hilo"""
+
+class Hilo:
+    """Define una clase llamada Hilo que representa un producto, en este caso hilo."""
+
+    def buscar_novedades(self):
+        """Busca nuevas novedades sobre este producto."""
+        print("Buscando novedades en el hilo")
+
+    # Instancia de la clase Hilo
+hilo1 = Hilo()
+
+# uso del metodo
+hilo1.buscar_novedades()
